a04dd6dd0e058adbf25355d6143e8b89c3f13790
Jose Antonio Barbero Aparicio <jba0040@alu.ubu.es>
2016-11-01 13:57:50 +0100

    Página Web base y login
    Closes #7
    
    Desarrollo de la página web básica y de un sistema de login para la
    misma.

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+$con= new mysqli('localhost', 'root', '', 'torneos2x2db');
+if ($con->connect_error){
+	die('Fallo de conexiÃ³n');
+}else{
+	$email=$_POST["email"];
+	$password=$_POST["password"];
+	
+	$sql = "SELECT id FROM users WHERE email='$email' AND password='$password'";
+	$result = $con->query($sql);
+	if ($result->num_rows>0){
+		header('Location: torneos.html');
+	}else{
+		//TODO mostrar que no existen usuarios con esa combinación
+		header('Location: index.html');
+	}
+}
+?>
