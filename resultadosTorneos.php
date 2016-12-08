cb637cca7b9b84cd1d598c824b2a5180805cac5d
jba0040 <jba0040@alu.ubu.es>
2016-12-08 14:41:35 +0100

    Perfil: Torneos
    En el perfil del usuario ahora se pueden consultar los torneos en los
    que ha participado ese usuario.
    
    Closes #54

--- resultadosTorneos.php
+++ resultadosTorneos.php
@@ -1,45 +1,58 @@
 <?php
+	session_start();
 	$conn = mysqli_connect("localhost","root","","torneos2x2db");
+	$user = $_GET['user'];
 	if (mysqli_connect_errno())
 	{
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
 
-	$selectTorneos = "SELECT id, nombre, fechaFin FROM torneos WHERE finalizado=1 ORDER BY fechaFin";
+	if($user == 1){
+		$mail = $_SESSION['userEmail'];
+		$selectTorneos = "SELECT * FROM participantes INNER JOIN torneos ON participantes.idTorneo=torneos.id WHERE finalizado=1 AND correo='$mail'";
+	} else {
+		$selectTorneos = "SELECT id, nombre, fechaFin FROM torneos WHERE finalizado=1 ORDER BY fechaFin";
+	}
 	$result = $conn->query($selectTorneos);
 
 	$divHtml ='';
 	while($row = $result->fetch_assoc()) {
-		$divHtml .=	'<a class="list-group-item">';
-		$divHtml .=	'<div class="panel panel-default">';
-		$divHtml .=	'<div class="panel-heading">' . $row["nombre"] . " - Fecha de finalización: " . $row["fechaFin"] . '</div>';
-		$divHtml .=	'<table class="table">';
-		$divHtml .=	'<thead>';
-		$divHtml .=	'<tr>';
-      	$divHtml .=	'<th>Puntuación</th>';
-      	$divHtml .=	'<th>Estrategia</th>';
-        $divHtml .=	'<th>Nombre</th>';
-        $divHtml .=	'<th>Apellidos</th>';
-        $divHtml .=	'<th>Email</th>';
-		$divHtml .=	'</tr>';
-		$divHtml .=	'</thead>';
-		$divHtml .=	'<tbody>';
+		$tempHtml = '';
+		$tempHtml .='<a class="list-group-item">';
+		$tempHtml .='<div class="panel panel-default">';
+		$tempHtml .=	'<div class="panel-heading">' . $row["nombre"] . " - Fecha de finalización: " . $row["fechaFin"] . '</div>';
+		$tempHtml .='<table class="table">';
+		$tempHtml .='<thead>';
+		$tempHtml .='<tr>';
+      	$tempHtml .='<th>Puntuación</th>';
+      	$tempHtml .='<th>Estrategia</th>';
+        $tempHtml .='<th>Nombre</th>';
+        $tempHtml .='<th>Apellidos</th>';
+        $tempHtml .='<th>Email</th>';
+		$tempHtml .='</tr>';
+		$tempHtml .='</thead>';
+		$tempHtml .='<tbody>';
 		$idTorneo = $row["id"];
 		$selectParticipantes = "SELECT nombre, apellidos, nombreEstrategia, correo, puntuacion FROM participantes WHERE idTorneo='$idTorneo' ORDER BY puntuacion DESC";
 		$result2 = $conn->query($selectParticipantes);
+		$torneoVacio = true;
 		while($row2 = $result2->fetch_assoc()) {
-			$divHtml .=	'<tr>';
-			$divHtml .=	'<td>' . $row2["puntuacion"] . '</td>';
-			$divHtml .=	'<td>' . $row2["nombreEstrategia"] . '</td>';
-			$divHtml .=	'<td>' . $row2["nombre"] . '</td>';
-			$divHtml .=	'<td>' . $row2["apellidos"] . '</td>';
-			$divHtml .=	'<td>' . $row2["correo"] . '</td>';
-			$divHtml .=	'</tr>';
+			$torneoVacio = false;
+			$tempHtml .='<tr>';
+			$tempHtml .='<td>' . $row2["puntuacion"] . '</td>';
+			$tempHtml .='<td>' . $row2["nombreEstrategia"] . '</td>';
+			$tempHtml .='<td>' . $row2["nombre"] . '</td>';
+			$tempHtml .='<td>' . $row2["apellidos"] . '</td>';
+			$tempHtml .='<td>' . $row2["correo"] . '</td>';
+			$tempHtml .='</tr>';
 		}
-		$divHtml .=	'</tbody>';
-		$divHtml .=	'</table>';
-		$divHtml .=	'</div>';
-		$divHtml .=	'</a>';
+		$tempHtml .='</tbody>';
+		$tempHtml .='</table>';
+		$tempHtml .='</div>';
+		$tempHtml .='</a>';
+		if(!$torneoVacio){
+			$divHtml .= $tempHtml;
+		}
 	}
 	echo $divHtml;
 	$conn->close();
