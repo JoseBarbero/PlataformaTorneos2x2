f2ffa6622d93808aac02152764ca37289f929656
jba0040 <jba0040@alu.ubu.es>
2017-01-08 00:06:44 +0100

    Licencia
    Selección de la licencia del proyecto y especificación de la misma en
    los ficheros correspondientes.
    
    Closes #86

--- src/php/parametrosSesion.php
+++ src/php/parametrosSesion.php
@@ -1,4 +1,30 @@
 <?php
+/*
+Plataforma Torneos 2x2
+Plataforma Torneos 2x2 es una aplicación destinada a la simulación
+de torneos de tipo 2x2 en una plataforma web.
+Copyright (C) 2017 Jose Antonio Barbero
+
+This file is part of Plataforma Torneos 2x2.
+
+Plataforma Torneos 2x2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Plataforma Torneos 2x2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Plataforma Torneos 2x2.  If not, see <http://www.gnu.org/licenses/>.
+
+Contact information:
+Jose Antonio Barbero
+   University of Burgos, Spain. 
+   e-mail: jba0040@alu.ubu.es
+*/
 	session_start();
 	$parametroSesion = $_GET['parametroSesion'];
 	if($parametroSesion == "nombre"){
